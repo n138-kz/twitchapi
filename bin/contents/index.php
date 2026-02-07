4635796599979569cd4eaa7683af61a0553f409a
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-02-07 14:27:35 +0900

    Update index.php

--- bin/contents/index.php
+++ bin/contents/index.php
@@ -4,4 +4,5 @@ header('content-type: text/plain');
 $config=[
     'filepath'=>realpath(__DIR__.'/../config.json'),
 ];
+$config['rawdata']=file_get_contents($config['filepath']);
 echo json_encode($config);

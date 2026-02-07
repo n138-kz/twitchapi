5c1445c8122c33672de464087b470f4d0cca854c
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-02-07 15:50:10 +0900

    Update index.php

--- bin/contents/index.php
+++ bin/contents/index.php
@@ -7,7 +7,7 @@ $config=[
 $config['data']=[];
 $config['data']['raw']=file_get_contents($config['filepath']);
 $config['data']['parse']=json_decode($config['data']['raw']);
-$config['export_format']=(explode(';', strtolower($_SERVER['CONTENT_TYPE']).';')[0]=='application/json')?'application/json':'text/plain';
+$config['export_format']=(isset($_SERVER['CONTENT_TYPE'])&&explode(';', strtolower($_SERVER['CONTENT_TYPE']).';')[0]=='application/json')?'application/json':'text/plain';
 
 if(!isset($_SERVER['REQUEST_METHOD'])||strtoupper($_SERVER['REQUEST_METHOD'])!='GET'){
 	http_response_code(405);

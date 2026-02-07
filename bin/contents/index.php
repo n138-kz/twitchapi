1534d048f9bd8706c01c2659a2009c0a520f6aea
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-02-07 14:50:09 +0900

    Update index.php

--- bin/contents/index.php
+++ bin/contents/index.php
@@ -7,7 +7,7 @@ $config=[
 $config['data']=[];
 $config['data']['raw']=file_get_contents($config['filepath']);
 $config['data']['parse']=json_decode($config['data']['raw']);
-echo json_encode(['$config'=>$config,'$_SERVER'=>$_SERVER]);
+echo json_encode(['config'=>$config,'_SERVER'=>$_SERVER]);
 if(!isset($_SERVER['REQUEST_METHOD'])||strtoupper($_SERVER['REQUEST_METHOD'])!='GET'){
 	http_response_code(405);
 	die('Invalid method');

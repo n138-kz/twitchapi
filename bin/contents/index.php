8f7610293a64907e2387ab3b79181316fe2eaf86
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-02-07 14:24:55 +0900

    Update index.php

--- bin/contents/index.php
+++ bin/contents/index.php
@@ -2,6 +2,6 @@
 header('content-type: text/plain');
 //http_response_code(204);
 $config=[
-    'filepath'=>__DIR__'/../config.json',
+    'filepath'=>__DIR__.'/../config.json',
 ];
 echo json_encode($config);

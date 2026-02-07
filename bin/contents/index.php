03eb09e0dc469eb58fffada1c9279c6dd79e0171
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-02-07 14:46:42 +0900

    Update index.php

--- bin/contents/index.php
+++ bin/contents/index.php
@@ -1,10 +1,17 @@
 <?php
-header('content-type: text/plain');
-//http_response_code(204);
+header('content-type: text/plain;charset=UTF-8');
+// http_response_code(204);
 $config=[
     'filepath'=>realpath(__DIR__.'/../config.json'),
 ];
 $config['data']=[];
 $config['data']['raw']=file_get_contents($config['filepath']);
 $config['data']['parse']=json_decode($config['data']['raw']);
-echo json_encode($config);
+echo json_encode(['$config'=>$config,'$_SERVER'=>$_SERVER]);
+if(!isset($_SERVER['REQUEST_METHOD'])||strtoupper($_SERVER['REQUEST_METHOD'])!='GET'){
+	http_response_code(405);
+	die('Invalid method');
+}
+if(!isset($_GET)){
+die('Missing item');
+}

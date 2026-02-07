763ca76a5fd585a2382eb9d7fa274b09813941ea
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-02-07 14:10:30 +0900

    Update index.php

--- bin/index.php
+++ bin/index.php
@@ -3,6 +3,6 @@ header('content-type:application/json;UTF8');
 $ls=[];
 foreach(glob('*') as $k1 => $v1){
 	if(!is_dir($v1)){continue;}
-	$ls[]=dirname($_SERVER['SCRIPT_URI']).$v1.'/';
+	$ls[]=dirname($_SERVER['SCRIPT_URI']).'/'.$v1.'/';
 }
 die(json_encode($ls));

2f7425bbe242abbbb620b13926117efb8067efba
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-02-18 04:51:37 +0900

    33 毎時0分にmarker自動でつける (#39)
    * Create compose.yaml
    
    * Create Dockerfile
    
    * Create entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update Dockerfile

---
+++ 3-setAutoMarker/twitch_bot_markers/entrypoint.sh
@@ -0,0 +1,24 @@
+while :;do
+	TZ='Asia/Tokyo' date;
+	if false;then
+		:
+	elif [ -z "${user_id}" -o "${user_id}" == "0" ];then
+		echo 'Missing user_id';
+	elif [ -z "${client_id}" -o "${client_id}" == "0" ];then
+		echo 'Missing client_id';
+	elif [ -z "${access_code}" -o "${access_code}" == "0" ];then
+		echo 'Missing access_code';
+	else
+		if [ $(date +%M) == '00' -a $(date +%S) == '00' ]; then
+			curl \
+				-s \
+				-X POST \
+				-H "Authorization: Bearer ${access_code}" \
+				-H "Client-Id: ${client_id}" \
+				-H 'Content-type: application/json' \
+				-d "$(jq -n --arg desc "$(TZ='Asia/Tokyo' date -R)" --arg user "${user_id}" '{user_id: $user, description: $desc}')" \
+				'https://api.twitch.tv/helix/streams/markers' | jq;
+		fi;
+	fi
+	sleep 1;
+done

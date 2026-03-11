ff167c68c6d3a612bb52943f3deb633448f8a9a6
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-03-12 06:09:28 +0900

    存在しないリクエストパラメータ(item)のとき404

--- 2-html/contents/index.php
+++ 2-html/contents/index.php
@@ -258,6 +258,16 @@ switch($request['item']){
 		$result=count($result)==1?$result[0]:$result;
 		break;
 	default:
+		http_response_code(404);
+		if(explode(';', $config['export_format'].';')[0]=='application/json'){
+			die(json_encode([
+				'request_at'=>$_SERVER['REQUEST_TIME'],
+				'status'=>http_response_code(),
+				'message'=>'Invalid item',
+			]));
+		} else {
+			die('Invalid item');
+		}
 }
 
 header("Content-Type: {$config['export_format']};charset=UTF-8");

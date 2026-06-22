6e868be10130b928b848bff815ba81df5071fd31
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-06-22 22:50:29 +0900

    Update getTwitchStreamTitle.py

--- 4-GetTwitchStreamTitleSetToOBS/getTwitchStreamTitle.py
+++ 4-GetTwitchStreamTitleSetToOBS/getTwitchStreamTitle.py
@@ -14,15 +14,21 @@ async def twitch_get_user(twitch):
     user = await first(twitch.get_users(logins=secret['twitch']['logins']))
     return user
 
+async def twitch_get_streamsettings(twitch, broadcaster_id=None):
+    stream_settings = await twitch.get_channel_information(broadcaster_id=broadcaster_id)
+    if isinstance(stream_settings, list) and len(stream_settings) > 0:
+        stream_settings = stream_settings[0]
+    return stream_settings
+
 async def twitch_example():
     twitch = await Twitch(secret['twitch']['client_id'], secret['twitch']['client_secret'])
     user = await twitch_get_user(twitch)
 
-    stream = await first(twitch.get_streams(user_id=[user.id]))
-    if stream:
-        print(stream.title)
+    channel_info = await twitch_get_streamsettings(twitch, broadcaster_id=user.id)
+    if channel_info:
+        print(channel_info.title)
     else:
-        print("配信はオフラインです。")
+        print("チャンネル情報が見つかりませんでした。")
     await twitch.close()
 
 #実行
